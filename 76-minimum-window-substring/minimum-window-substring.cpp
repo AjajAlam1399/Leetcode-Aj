2643dec9001f5a328a32d1782ddab51e990c2ff2
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-02-04 19:11:40 +0530

    Time: 26 ms (24.61%) | Memory: 10.8 MB (5.28%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    string minWindow(string S, string t) {
+        unordered_map<char,int>map1;
+        unordered_map<char,int>map2;
+        int n1=t.size();
+        int n2=S.size();
+        for(int i=0;i<n1;i++){
+            map1[t[i]]++;
+        }
+        int s=-1,e=-1,len=1e7;
+        int i=-1,j=-1;
+        int cnt=0;
+        while(true){
+            bool f1=false;
+            bool f2=false;
+            while(i<n2-1){
+                f1=true;
+                i++;
+                if(map1.find(S[i])!=map1.end() && map2[S[i]]<map1[S[i]]){
+                    cnt++;
+                }
+                map2[S[i]]++;
+                if(cnt==n1){
+                    break;
+                }
+            }
+
+            while(j<i){
+                f2=true;
+                j++;
+                if(cnt==n1){
+                    int currlen=i-j+1;
+                    if(len>currlen){
+                        len=currlen;
+                        s=j;
+                        e=i;
+                    }
+                }
+
+                if(map1.find(S[j])!=map1.end()){
+                    if(map2[S[j]]>map1[S[j]]){
+                        erase(S[j],map2);
+                    }
+                    else{
+                        cnt--;
+                         erase(S[j],map2);
+                    }
+                }
+                else{
+                     erase(S[j],map2);
+                }
+                if(cnt<n1){
+                    break;
+                }
+
+            }
+            if(!f1 && !f2)break;
+        }
+        return len==1e7?"":S.substr(s,e-s+1);
+    }
+
+    void erase(char ch,unordered_map<char,int>&map){
+        if(map[ch]==1){
+            map.erase(ch);
+        }
+        else{
+            map[ch]--;
+        }
+    }
+};
