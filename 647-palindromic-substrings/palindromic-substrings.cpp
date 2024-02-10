a09d2ffde119daa807939c12b5fa1afba602cd69
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-02-10 21:40:17 +0530

    Time: 20 ms (30.88%) | Memory: 9.4 MB (59.26%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -3,12 +3,27 @@ public:
     int countSubstrings(string s) 
     {
         int cnt=0;
-        for(int i=0;i<s.size();i++){
-            for(int j=i;j<s.size();j++){
-                if(isValid(s,i,j)){
-                    cnt++;
+        int n=s.size();
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+
+        for(int g=0;g<n;g++){
+            for(int i=0,j=g;j<n;i++,j++){
+                if(g==0){
+                    dp[i][j]=true;
                 }
+                else if(g==1){
+                    dp[i][j]=s[i]==s[j];
+                }
+                else{
+                    if(s[i]==s[j] && dp[i+1][j-1]==true){
+                        dp[i][j]=true;
+                    }
+                }
+                if(dp[i][j]){
+                cnt++;
             }
+            }
+            
         }
         return cnt;
     }
