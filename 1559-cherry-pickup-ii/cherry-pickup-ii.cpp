04abd75ee2762736d89a59b6c23bc80c13035a8c
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-02-11 18:10:40 +0530

    Time: 42 ms (90.61%) | Memory: 19.3 MB (18.05%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -3,10 +3,6 @@ public:
     int fun(vector<vector<int>>& grid,int row,int col1,int
      col2,vector<vector<vector<int>>>&dp){
 
-         if(col1<0 || col2<0 || col1>=grid[0].size()
-         ||col2>=grid[0].size()){
-             return -1e8;
-         }
 
          if(row==grid.size()-1){
              if(col1==col2){
@@ -23,9 +19,11 @@ public:
 
          int max_val=0;
          for(int i=-1; i<=1;i++){
-             for(int j=-1;j<=1;j++){
                  int cher;
-                 if(col1==col2){
+             for(int j=-1;j<=1;j++){
+                 if(col1+i>=0 && col2+j>=0 && col1+i<grid[0].size()
+                  && col2+j<grid[0].size()){
+                      if(col1==col2){
                      cher=grid[row][col1]+fun(grid,row+1,col1+i,
                      col2+j,dp);
                  }
@@ -34,6 +32,7 @@ public:
                      fun(grid,row+1,col1+i,col2+j,dp);
                  }
                  max_val=max(max_val,cher);
+                  }
              }
          }
          return dp[row][col1][col2]= max_val;
