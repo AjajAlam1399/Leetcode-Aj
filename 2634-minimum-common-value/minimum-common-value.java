23727d6a6f11811b9f1ecd0b5b4d612c97bfc972
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-03-09 10:45:21 +0530

    Time: 13 ms (28.52%) | Memory: 63.7 MB (19.85%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,30 +1,17 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         int ans=-1;
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            map.put(nums1[i],1);
+        }
         for(int i=0;i<nums2.length;i++ ){
-            if(fun(nums1,nums2[i])){
+            if(map.containsKey(nums2[i])){
                 ans=nums2[i];
                 break;
             }
         }
 
         return ans;
-    }
-    boolean fun(int nums[],int val){
-        int s=0,e=nums.length-1;
-        while(s<=e){
-            int mid=(s+e)/2;
-            if(nums[mid]==val){
-                return true;
-            }
-            else if(nums[mid]>val){
-                e=mid-1;
-            }
-            else{
-                s=mid+1;
-            }
-        }
-
-        return false;
     }
 }
