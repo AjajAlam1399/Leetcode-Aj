05331a86b5a292cfdc6d715af14f8d06b5a3e01c
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-03-12 17:15:26 +0530

    Time: 119 ms (52.28%) | Memory: 94.2 MB (34.49%) - LeetSync

---
+++ 1814-jump-game-vi/jump-game-vi.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n=nums.size();
+        int ans=nums[0];
+        priority_queue<pair<int,int>>pq;
+        pq.push({nums[0],0});
+        for(int i=1;i<n;i++){
+            while(!pq.empty() && pq.top().second<i-k){
+                pq.pop();
+            }
+            ans=pq.top().first+nums[i];
+            pq.push({ans,i});
+        }
+
+        return ans;
+    }
+};
