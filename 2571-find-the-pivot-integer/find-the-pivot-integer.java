ad6d8a6d3a0fc6de500de568dd7b475f2d3600bf
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-03-13 09:04:49 +0530

    Time: 1 ms (77.54%) | Memory: 40.4 MB (67.67%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int totalsum=((n)*(n+1))/2;
+        // System.out.println(totalsum);
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+            int sum2=totalsum-sum+i;
+            if(sum==sum2){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
