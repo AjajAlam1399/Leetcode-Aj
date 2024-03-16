b0f8eda9df28a538f0f2b0cd3167d35be133ea9b
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-03-16 11:24:02 +0530

    Time: 710 ms (10.00%) | Memory: 113 MB (5.00%) - LeetSync

---
+++ 2114-minimum-number-of-work-sessions-to-finish-the-tasks/minimum-number-of-work-sessions-to-finish-the-tasks.java
@@ -0,0 +1,38 @@
+class Solution {
+    int workSession = 0;
+
+    public int minSessions(int[] tasks, int sessionTime) {
+        workSession = sessionTime;
+        int n=tasks.length;
+        int[][][]dp=new int[n+2][sessionTime+1][1<<n+1];
+        for(int i=0;i<n+2;i++){
+            for(int j=0;j<=sessionTime;j++){
+
+            Arrays.fill(dp[i][j],-1);
+            }
+        }
+        return 1+fun(-1,0,0,sessionTime,tasks,dp);
+    }
+
+    int fun(int ind,int level,int mask,int sessionTime,int[] tasks,int[][][]dp){
+        if(level==tasks.length){
+            return 0;
+        }
+        if(dp[ind+1][sessionTime][mask]!=-1){
+            return dp[ind+1][sessionTime][mask];
+        }
+        int ans=tasks.length;
+        for(int i=0;i<tasks.length;i++){
+            if((mask&(1<<i))>0){
+                continue;
+            }
+            if(ind==-1||sessionTime>=tasks[i]){
+                ans=Math.min(ans,fun(i,level+1,(mask|(1<<i)),sessionTime-tasks[i],tasks,dp));
+            }
+            else{
+                ans=Math.min(ans,1+fun(i,level+1,(mask|1<<i),workSession-tasks[i],tasks,dp));
+            }
+        }
+        return dp[ind+1][sessionTime][mask]=ans;
+    }
+}
