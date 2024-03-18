086a883616367f88c68086f5cbd9caefe66fd16f
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-03-18 09:53:52 +0530

    Time: 242 ms (87.13%) | Memory: 93.4 MB (41.78%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,33 +1,21 @@
+bool comp(vector<int> &a, vector<int> &b){
+    return a[1] < b[1];
+}
 class Solution {
-    using pp=pair<int,int>;
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end());
-        priority_queue<pp,vector<pp>,greater<pp>>pq;
 
-        int n=points.size(),i=0;
-        int cnt=0;
-        int start=INT_MIN;
-
-        while(i<n || !pq.empty()){
-            if(pq.empty()){
-                pq.push({points[i][0],points[i][1]});
-                start=max(start,pq.top().second);
-                i++;
+        if(points.size() == 0) 
+            return 0; 
+        sort(points.begin(), points.end(), comp);
+        int arrows = 1;
+        int last = points[0][1];
+        for(int i = 1; i < points.size(); i++){
+            if(points[i][0] > last){
+                arrows++;
+                last = points[i][1];
             }
-            while(i<n && points[i][0]<=start){
-                pq.push({points[i][0],points[i][1]});
-                i++;
-            }
-            while(!pq.empty() && pq.top().first<=start){
-                start=min(start,pq.top().second);
-                pq.pop();
-            }
-            if(!pq.empty()){
-                start=max(start,pq.top().second);
-            }
-            cnt++;
         }
-        return cnt;
+        return arrows;
     }
 };
