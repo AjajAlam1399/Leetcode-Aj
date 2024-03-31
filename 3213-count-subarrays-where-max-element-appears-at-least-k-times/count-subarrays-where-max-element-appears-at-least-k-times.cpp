dcf731de08c0a72f89fe35ee63cbbd8f073b6dd5
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-03-31 09:04:42 +0530

    Time: 131 ms (50.37%) | Memory: 120.1 MB (33.48%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
-        long long int max_num = *max_element(nums.begin(),nums.end()),count = 0;
-        long long int left = 0,right = 0,ans = 0;
-        while(right<nums.size()){
-            if(nums[right] == max_num)count++;
-            
-            while(count>=k){
-                if(nums[left]==max_num)count--;
-                left++;
-                ans += nums.size()-right;
-            }    
-            right++;
+        long long  ans=0;
+        int i=-1,j=-1,n=nums.size();
+        long long cnt=0;
+        int maxel=*max_element(nums.begin(),nums.end());
+        while(true){
+            bool f1=false;
+            bool f2=false;
+            while(i<n-1){
+                f1=true;
+                i++;
+                if(nums[i]==maxel){
+                    cnt++;
+                }
+                if(cnt>=k){
+                    break;
+                }
+            }
+
+            while(j<i){
+                f2=true;
+                if(cnt>=k){
+                    ans+=n-i;
+                }
+                j++;
+                if(nums[j]==maxel){
+                    cnt--;
+                }
+                if(cnt<k){
+                    break;
+                }
+            }
+
+            if(!f1 && !f2){
+                break;
+            }
         }
-
-
         return ans;
-        
     }
 };
