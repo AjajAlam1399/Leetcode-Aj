7819232f279e72d03ce6df57f496d2089a893f29
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-04-19 09:23:37 +0530

    Time: 29 ms (55.47%) | Memory: 17.2 MB (39.44%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,80 @@
+class DisJointSet{
+    private:
+    vector<int>parent;
+    vector<int>size;
+
+    public:
+    DisJointSet(int v){
+        parent.resize(v);
+        size.resize(v);
+        for(int i=0;i<v;i++)parent[i]=i;
+    }
+    int findParent(int v){
+        if(parent[v]==v){
+        return parent[v];
+        }
+            return parent[v]=findParent(parent[v]);
+    }
+
+    void groupBySize(int x,int y){
+        int rootx=findParent(x);
+        int rooty=findParent(y);
+         if(rootx==rooty){
+            return ;
+        }
+        if(size[rootx]<size[rooty]){
+            parent[rootx]=rooty;
+            size[rooty]+=size[rootx];
+        }
+        else{
+            parent[rooty]=rootx;
+            size[rootx]+=size[rooty];
+        }
+    }
+    // int findSize(int ind){
+    //     return size[];
+    // }
+
+};
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int row=grid.size();
+        int col=grid[0].size();
+        DisJointSet ds(row*col+1);
+
+        int cnt=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]=='1'){
+                    int arr1[]={-1,0,1,0};
+                    int arr2[]={0,-1,0,1};
+                    for(int dir=0;dir<4;dir++){
+                        int newi=i+arr1[dir];
+                        int newj=j+arr2[dir];
+                        if(newi>=0 && newj>=0 && newi<row && newj<col && grid[newi][newj]=='1'){
+                            int ind1=col*i+j;
+                            int ind2=col*newi+newj;
+                            ds.groupBySize(ind1,ind2);
+                        }
+                    }
+                }
+            }
+        }
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]=='1' ){
+                    int ind=col*i+j;
+                    if(ds.findParent(ind)==ind){
+                        cnt++;
+                    }
+                }
+            }
+            // cout<<endl;
+        }
+
+        return cnt;
+    }
+};
