2dbd3b072789aa87db648eb6310a9bb8161a926e
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-05-20 19:09:59 +0530

    Time: 3 ms (37.27%) | Memory: 40.7 MB (58.25%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,22 +1,29 @@
 class Solution {
     int sum;
+    int bitmask;
+
     public int subsetXORSum(int[] nums) {
-        sum=0;
-        ArrayList<Integer>list=new ArrayList<>();
-        fun(0,nums,list);
+        sum = 0;
+        bitmask=0;
+        fun(0, nums);
         return sum;
     }
-    void fun(int ind,int nums[],ArrayList<Integer>list){
-        if(ind==nums.length){
-            int curr=0;
-            for(int num:list)curr^=num;
-            sum+=curr;
+
+    void fun(int ind, int nums[]) {
+        if (ind == nums.length) {
+            int curr = 0;
+            for (int i = 0; i <nums.length; i++) {
+                int bit = ((bitmask) & (1<<i));
+                if (bit!=0) {
+                    curr ^= nums[i];
+                }
+            }
+            sum += curr;
             return;
         }
-        
-        fun(ind+1,nums,list);
-        list.add(nums[ind]);
-        fun(ind+1,nums,list);
-        list.remove(list.size()-1);
+        fun(ind + 1, nums);
+        bitmask |= (1 << ind);
+        fun(ind + 1, nums);
+        bitmask&=~(1 << ind);
     }
 }
