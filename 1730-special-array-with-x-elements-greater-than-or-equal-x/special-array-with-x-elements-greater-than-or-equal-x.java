c689f639a7a90406fec00721060b3df50cc9e451
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-05-27 10:38:11 +0530

    Time: 3 ms (13.96%) | Memory: 40.9 MB (83.20%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            int numRight=nums.length-i;
+            int cnt=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]>=numRight)cnt++;
+            }
+            if(cnt==numRight)return cnt;
+        }
+        return -1;
+    }
+}
