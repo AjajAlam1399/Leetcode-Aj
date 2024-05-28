71d3c281895da882e1828151780ac53561785162
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-05-28 13:36:18 +0530

    Time: 197 ms (27.11%) | Memory: 199 MB (5.75%) - LeetSync

---
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
+        unordered_map<int,vector<int>>vec;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            vec[nums[i]].push_back(i);
+        }
+
+        vector<int>ans;
+        
+            auto it=vec[x];
+
+        for(int i=0;i<queries.size();i++){
+            if(it.size()<queries[i]){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(it[queries[i]-1]);
+            }
+        }
+
+        return ans;
+    }
+};
