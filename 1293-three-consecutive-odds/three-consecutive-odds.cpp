89ccb462392115bac3c25049d0e68579561baf37
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-07-01 20:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (45.76%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -3,7 +3,7 @@ public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         int n=arr.size();
         for(int i=2;i<n;i++){
-            if(arr[i-2]%2!=0 && arr[i-1]%2!=0 && arr[i]%2!=0)return true;
+            if(arr[i-2]&1&& arr[i-1]&1 && arr[i]&1)return true;
         }
         return false;
     }
