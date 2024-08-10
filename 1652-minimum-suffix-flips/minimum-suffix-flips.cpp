9db7654b7843e9a69a81ec91a140e08f10c2fc46
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-08-10 19:50:12 +0530

    Time: 14 ms (58.71%) | Memory: 11 MB (19.86%) - LeetSync

--- 1652-minimum-suffix-flips/minimum-suffix-flips.cpp
+++ 1652-minimum-suffix-flips/minimum-suffix-flips.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int minFlips(string target) {
-        int ans=0;
-        for(int i=0;i<target.size();i++){
-            if(target[i]=='1' && ans%2==0)ans++;
-            else if(target[i]=='0' && ans%2!=0)ans++;
+        int cnt=0,n=target.size();
+
+        for(int i=0;i<n;i++){
+            int bit=target[i]-'0';
+            int modval=cnt%2;
+            if(modval!=bit){
+                cnt++;
+            }
         }
-        return ans;
+        return cnt;
     }
 };
