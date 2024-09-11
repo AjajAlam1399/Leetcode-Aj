debf1d61e2a9c475a60e3168fd74e4e965384d13
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-09-11 18:24:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (93.51%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int cnt=0;
+        for(int i=0;i<32;i++){
+            int bit1=(start)&(1<<i);
+            int bit2=(goal)&(1<<i);
+            if(bit1!=bit2)cnt++;
+        }
+        return cnt;
+    }
+};
