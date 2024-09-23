523016202a56a8e976b7f1bbfb38fb33c7214cf3
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-09-23 22:08:40 +0530

    Time: 121 ms (57.60%) | Memory: 76.6 MB (63.18%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    int minExtraChar(string s, vector<string>& dict) {
+    int minExtraChar(string s, vector<string>& dictionary) {
         unordered_set<string>set;
-
-        for(int i=0;i<dict.size();i++){
-            set.insert(dict[i]);
+        int n=s.size();
+        for(auto it :dictionary){
+            set.insert(it);
         }
-
-        vector<int>dp(s.size()+1,-1);
-
+        vector<int>dp(n+1,-1);
         return fun(0,s,set,dp);
     }
 
-    int fun(int ind,string str,unordered_set<string>&set,vector<int>&dp){
-        if(ind>=str.size()){
+    int fun(int ind,string &s,unordered_set<string>&set,vector<int>&dp){
+
+        if(ind==s.size()){
             return 0;
         }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int ans=1e5;
-        string st="";
-        for(int i=ind;i<str.size();i++){
-           st+=str[i];
-            int currextr=0;
-            if(set.find(st)==set.end()){
-                currextr=st.size();;
+
+        string str="";
+        int ans=s.size();
+        if(dp[ind]!=-1)return dp[ind];
+        for(int i=ind;i<s.size();i++){
+            str.push_back(s[i]);
+            int len=str.size();
+            if(set.find(str)!=set.end()){
+                ans=min(ans,0+fun(i+1,s,set,dp));
             }
-            int remaning=fun(i+1,str,set,dp);
-
-            ans=min(ans,currextr+remaning);
+            ans=min(ans,len+fun(i+1,s,set,dp));
         }
-
         return dp[ind]=ans;
     }
 };
