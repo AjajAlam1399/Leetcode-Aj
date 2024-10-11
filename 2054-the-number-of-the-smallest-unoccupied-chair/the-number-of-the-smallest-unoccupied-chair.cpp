ade437832ca2f22ec2e5abdd07dcc38e35fff32c
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-10-11 09:30:41 +0530

    Time: 405 ms (7.56%) | Memory: 72 MB (23.71%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
-        int chair=0;
+    
+        int n=times.size();
         vector<vector<int>>vec;
         for(int i=0;i<times.size();i++){
             vec.push_back({times[i][0],times[i][1],i});
         }
         sort(vec.begin(),vec.end());
-        unordered_map<int,int>map;
-        priority_queue<int,vector<int>,greater<int>>pq;
+        vector<int>ch(n);
 
         for(int i=0;i<vec.size();i++){
-
             int s=vec[i][0];
             int e=vec[i][1];
             int pos=vec[i][2];
-            vector<int>erase;
-            for(auto it : map){
-                if(it.second<=s){
-                    pq.push(it.first);
-                    erase.push_back(it.first);
+            int chair;
+            for(int i=0;i<times.size();i++){
+                if(ch[i]==0){
+                    ch[i]=e;
+                    chair=i;
+                    break;
+                }
+                else if(ch[i]<=s){
+                    ch[i]=e;
+                    chair=i;
+                    break;
                 }
             }
-            for(auto it : erase){
-                map.erase(it);
-            }
-            int currchair=chair;
-            if(!pq.empty()){
-                currchair=pq.top();
-                pq.pop();
-            }
-            else{
-                chair++;
-            }
             if(pos==targetFriend){
-                return currchair;
+                return chair;
             }
-            map[currchair]=e;
         }
-        return chair;
+        return 0;
+
     }
 };
