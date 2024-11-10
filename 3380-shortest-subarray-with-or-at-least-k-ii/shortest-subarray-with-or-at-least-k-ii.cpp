f5872d56a1b6b25c05de128f347f678bf0278196
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-11-10 10:46:41 +0530

    Time: 37 ms (90.11%) | Memory: 88.3 MB (44.57%) - LeetSync

--- 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
@@ -2,50 +2,35 @@ class Solution {
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
         int n = nums.size();
-        int i = -1, j = -1;
         vector<int> vec(32, 0);
-        int curor = 0;
-        int len = INT_MAX;
-        while (true) {
-            bool f1 = false;
-            bool f2 = false;
-            while (i < n - 1) {
-                f1 = true;
-                i++;
-                int num = nums[i];
-                for (int b = 0; b < 32; b++) {
-                    int bit = ((num) & (1 << b));
-                    if (bit)
-                        vec[b]++;
-                }
-                curor |= num;
-                if (curor >= k) {
-                    break;
+        int ans = INT_MAX;
+        int s = 0, e = 0;
+        int orA = 0;
+        while (e < n) {
+            orA |= nums[e];
+            for (int i = 31; i >= 0; i--) {
+                int bit = ((nums[e]) & (1 << i));
+                if (bit) {
+                    vec[i]++;
                 }
             }
-            while (j < i) {
-                // cout << curor << " " << i << " " << j << endl;
-                f2 = true;
-                if (curor >= k) {
-                    len = min(len, i - j);
-                }
-                j++;
-                int num = nums[j];
-                for (int b = 0; b < 32; b++) {
-                    int bit = ((num) & (1 << b));
-                    if (bit) {
-                        if (vec[b] == 1) {
-                            curor -= (1 << b);
+            if (orA >= k) {
+                while (e>=s && orA >= k) {
+                    ans = min(ans, e - s + 1);
+                    for (int i = 31; i >= 0; i--) {
+                        int bit = ((nums[s]) & (1 << i));
+                        if (bit) {
+                            vec[i]--;
+                            if (!vec[i]) {
+                                orA ^= (1 << i);
+                            }
                         }
-                        vec[b]--;
                     }
+                    s++;
                 }
-                if (curor < k)
-                        break;
             }
-            if (!f1 && !f2)
-                break;
+            e++;
         }
-        return len == INT_MAX ? -1 : len;
+        return ans == INT_MAX ? -1 : ans;
     }
 };
