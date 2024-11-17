a3d8e9f688920d5f2d990dc6f14d293c21684907
AJAJ ALAM <2020umt1399@mnit.ac.in>
2024-11-17 18:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (67.61%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
@@ -1,15 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        return checker(root1, root2);
+        return fun(root1,root2);
     }
+    bool fun(TreeNode* root1,TreeNode* root2){
+        if(!root1 || !root2)return root1==root2;
+        if(root1->val!=root2->val)return false;
 
-private:
-    bool checker(TreeNode* node1, TreeNode* node2) {
-        if (!node1 && !node2) return true;
-        if (!node1 || !node2 || node1->val != node2->val) return false;
-
-        return (checker(node1->left, node2->left) || checker(node1->left, node2->right)) &&
-               (checker(node1->right, node2->right) || checker(node1->right, node2->left));
+        bool fun1=fun(root1->left,root2->left)&fun(root1->right,root2->right);
+        bool fun2=fun(root1->left,root2->right)&fun(root1->right,root2->left);
+        return fun1|fun2;
     }
 };
