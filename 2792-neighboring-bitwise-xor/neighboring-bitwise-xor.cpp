afc052ff5566d11374229e4f48887a53f689b1ad
AJAJ ALAM <2020umt1399@mnit.ac.in>
2025-01-21 15:46:15 +0530

    Time: 99 ms (5.21%) | Memory: 304.7 MB (5.44%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -5,8 +5,7 @@ public:
         if(derived.size()==1){
             return derived[0]==0;
         }
-        vector<vector<vector<int>>>dp(2,
-        vector<vector<int>>(2,vector<int>(n,-1)));
+        vector<vector<int>>dp(2,vector<int>(n,-1));
         bool ans=false;
         if(derived[0]){
             ans=fun(0,1,1,derived,dp)|fun(1,0,1,derived,dp);
@@ -18,23 +17,23 @@ public:
     }
 
     bool fun(int start,int last,int ind,vector<int>&derived,
-    vector<vector<vector<int>>>&dp){
+    vector<vector<int>>&dp){
         if(ind==derived.size()-1){
             return (last^start)==derived[ind];
         }
-        if(dp[start][last][ind]!=-1)return dp[start][last][ind];
+        if(dp[last][ind]!=-1)return dp[last][ind];
         if(derived[ind]){
             if(last){
-                return dp[start][last][ind] = fun(start,0,ind+1,derived,dp);
+                return dp[last][ind] = fun(start,0,ind+1,derived,dp);
             }
-            else return dp[start][last][ind]= fun(start,1,ind+1,derived,dp);
+            else return dp[last][ind]= fun(start,1,ind+1,derived,dp);
         }
         else {
             if(last){
-                return dp[start][last][ind] = fun(start,1,ind+1,derived,dp);
+                return dp[last][ind] = fun(start,1,ind+1,derived,dp);
             }
-            else return dp[start][last][ind] = fun(start,0,ind+1,derived,dp);
+            else return dp[last][ind] = fun(start,0,ind+1,derived,dp);
         }
-        return dp[start][last][ind] = false;
+        return dp[last][ind] = false;
     }
 };
