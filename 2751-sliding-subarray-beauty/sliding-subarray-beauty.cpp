ade5a25997326d8d47a103b252c05d05d5e72b3d
AJAJ ALAM <2020umt1399@mnit.ac.in>
2025-05-28 23:38:16 +0530

    Time: 46 ms (71.13%) | Memory: 190.8 MB (77.68%) - LeetSync

--- 2751-sliding-subarray-beauty/sliding-subarray-beauty.cpp
+++ 2751-sliding-subarray-beauty/sliding-subarray-beauty.cpp
@@ -1,42 +1,53 @@
 class Solution {
+
+    vector<int>bit;
+    void update(int idx,int val){
+        
+        while(idx<52){
+            bit[idx]+=val;
+            idx+=idx&(-idx);
+        }
+    }
+
+    int query(int x){
+        int currsum=0,currind=0;
+
+        for(int i=log2(52);i>=0;i--){
+            if(currind+(1<<i)<52 && currsum+bit[currind+(1<<i)]<x){
+                currind+=(1<<i);
+                currsum+=bit[currind];
+            }
+        }
+
+        return currind+1;
+    }
 public:
+    Solution(){
+        bit.resize(52,0);
+    }
     vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
-        int n = nums.size();
+        
+        int n=nums.size();
 
-        vector<int> vec(101, 0);
+        int i=0,j=0;
 
-        int i = 0, j = 0;
-        vector<int> ans(n-k+1,0);
-        int subcnt=0;
-        while (j < n) {
-            if (nums[j] >= 0) {
-                vec[nums[j] + 50]++;
-            } else {
-                vec[50 - abs(nums[j])]++;
+        vector<int>ans;
+
+        while(j<n){
+           if(nums[j]<0){
+            update(51-abs(nums[j]),1);
+           } 
+
+           if(j-i+1==k){
+            int q=query(x);
+            if(q>51)ans.push_back(0);
+            else ans.push_back(-1*(51-q));
+            if(nums[i]<0){
+                update(51-abs(nums[i]),-1);
             }
-
-            if (j - i + 1 == k) {
-                int cnt = 0;
-                for (int ind = 0; ind < 50; ind++) {
-                    if (vec[ind]){
-                        cnt+=vec[ind];
-                        // cout<<ind<<","<<vec[ind]<<endl;
-                    }
-                    if (cnt >=x) {
-                        ans[subcnt]=(-1 * (50 - ind));
-                        break;
-                    }
-                }
-                int val = nums[i];
-                if (val >= 0) {
-                    vec[val + 50]--;
-                } else {
-                    vec[50 - abs(val)]--;
-                }
-                i++;
-                subcnt++;
-            }
-            j++;
+            i++;
+           }
+           j++;
         }
 
         return ans;
