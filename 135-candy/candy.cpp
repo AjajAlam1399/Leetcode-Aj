e9ff93edbfe15b3068380ada1d6b68e20e357f5a
AJAJ ALAM <2020umt1399@mnit.ac.in>
2025-06-02 15:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (43.50%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,52 +2,20 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<vector<int>> graph(n);
-        vector<int> indegree(n, 0);
+        vector<int> candies(n, 1);
 
-        vector<int> count(n, 0);
-
-        for (int i = 0; i < ratings.size(); i++) {
-            int leftVal = i - 1 >= 0 ? ratings[i - 1] : INT_MIN;
-            int rightVal = i + 1 < n ? ratings[i + 1] : INT_MIN;
-
-            if (ratings[i] < leftVal) {
-                graph[i].push_back(i - 1);
-                indegree[i - 1]++;
-            }
-            if (ratings[i] <rightVal) {
-                graph[i].push_back(i + 1);
-                indegree[i + 1]++;
-            }
-        }
-        queue<int> q;
-
-        for (int i = 0; i < n; i++) {
-            if (!indegree[i]) {
-                count[i] = 1;
-                q.push(i);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
         }
 
-        while (!q.empty()) {
-            auto node = q.front();
-            q.pop();
-
-            for (auto adjNode : graph[node]) {
-                indegree[adjNode]--;
-                if (!indegree[adjNode]) {
-                    count[adjNode] = count[node] + 1;
-                    q.push(adjNode);
-                }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
             }
         }
 
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            ans += count[i];
-        }
-
-        return ans;
+        return accumulate(candies.begin(), candies.end(), 0);
     }
 };
