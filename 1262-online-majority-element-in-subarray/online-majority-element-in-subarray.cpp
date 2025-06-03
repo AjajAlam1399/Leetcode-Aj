f48533187dff4bfdb747b8d9b3ad4008c5f917d6
AJAJ ALAM <2020umt1399@mnit.ac.in>
2025-06-03 15:51:37 +0530

    Time: 101 ms (46.15%) | Memory: 112.2 MB (59.17%) - LeetSync

--- 1262-online-majority-element-in-subarray/online-majority-element-in-subarray.cpp
+++ 1262-online-majority-element-in-subarray/online-majority-element-in-subarray.cpp
@@ -1,43 +1,43 @@
 class MajorityChecker {
-private:
-    unordered_map<int, vector<int>> pos;
-    vector<int> a;
-    int try_bound;
-    
+    unordered_map<int,vector<int>>map;
+    vector<int>arr;
+    int n;
 public:
-    MajorityChecker(vector<int>& arr): a(arr) {
-        srand(time(nullptr));
-        for (int i = 0; i < arr.size(); ++i) {
-            pos[arr[i]].push_back(i);
+    MajorityChecker(vector<int>& arr) {
+        this->n=arr.size();
+        this->arr=arr;
+        for(int i=0;i<arr.size();i++){
+            map[arr[i]].push_back(i);
         }
-        try_bound = 20;
-    }
-    
-    int get_occurrence(int num, int l, int r) {
-        auto iter = pos.find(num);
-        if (iter == pos.end()) {
-            return 0;
-        }
-        const auto& vec = iter->second;
-        auto iter_l = lower_bound(vec.begin(), vec.end(), l);
-        if (iter_l == vec.end()) {
-            return 0;
-        }
-        auto iter_r = upper_bound(vec.begin(), vec.end(), r);
-        return iter_r - iter_l;
-    }
-    
-    int get_random(int l, int r) {
-        return rand() % (r - l + 1) + l;
     }
     
     int query(int left, int right, int threshold) {
-        for (int i = 0; i < try_bound; ++i) {
-            int elem = a[get_random(left, right)];
-            if (get_occurrence(elem, left, right) >= threshold) {
-                return elem;
+        for(int i=0;i<20;i++){
+            int num=arr[randInd(left,right)];
+            if(check(num,left,right)>=threshold){
+                return num;
             }
         }
+
         return -1;
     }
+
+    int check(int num,int l,int r){
+        auto &vec=map[num];
+        int lb=lower_bound(vec.begin(),vec.end(),l)-vec.begin();
+        if(lb==n)return 0;
+        int up=upper_bound(vec.begin(),vec.end(),r)-vec.begin();
+        
+        return up-lb;
+    }
+
+    int randInd(int l,int r){
+        return l+rand()%(r-l+1);
+    }
 };
+
+/**
+ * Your MajorityChecker object will be instantiated and called as such:
+ * MajorityChecker* obj = new MajorityChecker(arr);
+ * int param_1 = obj->query(left,right,threshold);
+ */
