4a0049f5972c29a3749f11570f580240c7c94345
AJAJ ALAM <2020umt1399@mnit.ac.in>
2025-07-12 12:11:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.88%) - LeetSync

--- 796-reaching-points/reaching-points.cpp
+++ 796-reaching-points/reaching-points.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     bool reachingPoints(int sx, int sy, int tx, int ty) {
         while (tx >= sx && ty >= sy) {
-            if (tx == sx && ty == sy) return true;
+            if (tx == sx) return (ty - sy) % sx == 0;
+            if (ty == sy) return (tx - sx) % sy == 0;
 
-            if (tx > ty) {
-                if (ty > sy)
-                    tx %= ty;
-                else
-                    return (tx - sx) % ty == 0;
-            } else {
-                if (tx > sx)
-                    ty %= tx;
-                else
-                    return (ty - sy) % tx == 0;
-            }
+            if (tx > ty)
+                tx %= ty;
+            else
+                ty %= tx;
         }
         return false;
     }
