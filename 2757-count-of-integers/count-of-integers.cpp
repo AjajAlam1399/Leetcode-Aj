5a456ae3d5d15f0b2e310491568c49fc0cf94a18
AJAJ ALAM <2020umt1399@mnit.ac.in>
2025-07-27 13:30:06 +0530

    Time: 16 ms (57.52%) | Memory: 9.4 MB (42.74%) - LeetSync

--- 2757-count-of-integers/count-of-integers.cpp
+++ 2757-count-of-integers/count-of-integers.cpp
@@ -1,17 +1,14 @@
 class Solution {
     using ll=long long;
     int mod=1e9+7;
+    int dp[23][2][200];
 public:
     int count(string num1, string num2, int min_sum, int max_sum) {
         
-    
-        vector<vector<vector<ll>>>dp1(23,vector<vector<ll>>(2,
-        vector<ll>(200,-1)));
-        vector<vector<vector<ll>>>dp2(23,vector<vector<ll>>(2,
-        vector<ll>(200,-1)));
-
-        ll high = fun(0,true,0,num2,min_sum,max_sum,dp1)%mod;
-        ll low = fun(0,true,0,num1,min_sum,max_sum,dp2)%mod;
+        memset(dp,-1,sizeof(dp));
+        ll high = fun(0,true,0,num2,min_sum,max_sum)%mod;
+        memset(dp,-1,sizeof(dp));
+        ll low = fun(0,true,0,num1,min_sum,max_sum)%mod;
 
         int digitSum=0;
 
@@ -26,7 +23,7 @@ public:
         return (high-low+mod)%mod;
     }
 
-    ll fun(ll ind, ll tight,ll sum,string & str,ll min,ll max,vector<vector<vector<ll>>>&dp){
+    ll fun(ll ind, ll tight,ll sum,string & str,ll min,ll max){
 
         if(ind==str.size()){
             return (sum>=min && sum<=max);
@@ -42,7 +39,7 @@ public:
         ll ans=0;
         for(ll d=0;d<=limit;d++){
             ll newTight = tight && d==limit;
-            ans=(ans%mod + fun(ind+1,newTight,sum+d,str,min,max,dp)%mod)%mod;
+            ans=(ans%mod + fun(ind+1,newTight,sum+d,str,min,max)%mod)%mod;
         }
         return dp[ind][tight][sum]=ans%mod;
     }
