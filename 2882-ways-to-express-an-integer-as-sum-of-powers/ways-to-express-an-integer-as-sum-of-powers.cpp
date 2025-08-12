ef64ff13d5245796446ee4fa60755e5c4fe2bc50
AJAJ ALAM <2020umt1399@mnit.ac.in>
2025-08-12 09:36:26 +0530

    Time: 474 ms (17.43%) | Memory: 14 MB (64.49%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,27 +1,34 @@
 class Solution {
     using ll=long long;
+    int dp[305][305];
     int mod=1e9+7;
 public:
     int numberOfWays(int n, int x) {
-        vector<vector<ll>>dp(n+1,vector<ll>(n+1,-1));
-        return fun(1,n,n,x,dp);
+        vector<int>vec;
+        for(int i=1;i<=n;i++){
+            vec.push_back(i);
+        }
+        memset(dp,-1,sizeof(dp));
+        return fun(0,n,x,vec)%mod;
     }
 
-    ll fun(ll i,ll target,int &n,int &x,vector<vector<ll>>&dp){
+    int fun(int ind,int target,int x , vector<int>&vec){
 
-        if(i>n){
+        if(ind==vec.size()){
             return target==0;
         }
 
-        if(dp[i][target]!=-1)return dp[i][target];
-
-        ll not_taken=fun(i+1,target,n,x,dp);
-        ll taken=0;
-        ll powValue=(ll)pow(i,x);
-        if(target>=powValue){
-            taken=fun(i+1,target-powValue,n,x,dp);
+        if(dp[ind][target]!=-1){
+            return dp[ind][target];
         }
 
-        return dp[i][target]=(taken%mod+not_taken%mod)%mod;
+        int not_taken=fun(ind+1,target,x,vec)%mod;
+        int taken=0;
+        ll powVal=1LL*pow(vec[ind],x);
+        if(powVal<=target){
+            taken=fun(ind+1,target-powVal,x,vec)%mod;
+        }
+
+        return dp[ind][target] = (taken+not_taken)%mod;
     }
 };
