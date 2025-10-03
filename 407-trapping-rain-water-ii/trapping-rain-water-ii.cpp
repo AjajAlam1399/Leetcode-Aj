56b224623a6a683f71e94cf0c914308380967bc9
AJAJ ALAM <2020umt1399@mnit.ac.in>
2025-10-03 11:57:35 +0530

    Time: 26 ms (69.56%) | Memory: 18 MB (74.95%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
@@ -1,53 +1,55 @@
 class Solution {
+    using pp= pair<int,pair<int,int>>;
     int arr1[4]={-1,0,1,0};
-    int arr2 [4]={0,-1,0,1};
+    int arr2[4]={0,-1,0,1};
 public:
-    int trapRainWater(vector<vector<int>>& heightMap) {
-        int n=heightMap.size(),m=heightMap[0].size();
-        vector<vector<bool>>visted(n,vector<bool>(m,false));
+    int trapRainWater(vector<vector<int>>& map) {
+        
+        int n=map.size();
+        int m=map[0].size();
 
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        vector<vector<int>>visted(n,vector<int>(m,0));
+
+        priority_queue<pp,vector<pp>,greater<pp>>pq;
 
         for(int i=0;i<m;i++){
-            pq.push({heightMap[0][i],0,i});
-            pq.push({heightMap[n-1][i],n-1,i});
+            pq.push({map[0][i],{0,i}});
             visted[0][i]=true;
+            pq.push({map[n-1][i],{n-1,i}});
             visted[n-1][i]=true;
         }
-        for(int i=0;i<n;i++){
-            pq.push({heightMap[i][0],i,0});
-            pq.push({heightMap[i][m-1],i,m-1});
-            visted[i][0]=true;
-            visted[i][m-1]=true;
+
+        for(int j=0;j<n;j++){
+            pq.push({map[j][0],{j,0}});
+            visted[j][0]=true;
+            pq.push({map[j][m-1],{j,m-1}});
+            visted[j][m-1]=true;
         }
 
         int ans=0;
 
         while(!pq.empty()){
-            auto it =pq.top();
+            auto it = pq.top();
             pq.pop();
-            int i=it[1];
-            int j=it[2];
-            int curr=it[0];
+            int currVal=it.first;
+            int x=it.second.first;
+            int y=it.second.second;
 
             for(int k=0;k<4;k++){
-                int newi=i+arr1[k];
-                int newj=j+arr2[k];
+                int newx=x+arr1[k];
+                int newy=y+arr2[k];
 
-                if(newi>=0 && newj>=0 && newi<n && newj<m && !visted[newi][newj]){
-                    int newval=heightMap[newi][newj];
-                    if(curr>newval){
-                        ans+=curr-newval;
-                        newval=curr;
+                if(newx>=0 && newy>=0 && newx<n && newy<m && !
+                visted[newx][newy]){
+                    visted[newx][newy]=true;
+                    if(currVal>map[newx][newy]){
+                        ans+=currVal-map[newx][newy];
                     }
-                    visted[newi][newj]=true;
-                    pq.push({newval,newi,newj});
+                    pq.push({max(currVal, map[newx][newy]), {newx, newy}});
                 }
             }
-
         }
 
         return ans;
-
     }
 };
