f538b6eed60b58ceca2ed59b4e8dd1effe7c9e83
AJAJ ALAM <2020umt1399@mnit.ac.in>
2025-10-10 11:22:40 +0530

    Time: 148 ms (33.10%) | Memory: 145.4 MB (88.38%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-        int n=energy.size();
-
-        vector<int>leftDp(n);
-        int ans=INT_MIN;
-
-        for(int i=0;i<n;i++){
-            if(i-k>=0){
-                leftDp[i]=leftDp[i-k]+energy[i];
-            }
-            else{
-                leftDp[i]=energy[i];
-            }
-
-            if(i+k>=n){
-                ans=max(ans,energy[i]);
-                ans=max(ans,leftDp[i]);
+        int n = energy.size(), ans = INT_MIN;
+        for (int i = n - k; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j >= 0; j -= k) {
+                sum += energy[j];
+                ans = max(ans, sum);
             }
         }
-
-        vector<int>rightDp(n);
-
-        for(int i=n-1;i>=0;i--){
-            if(i+k>=n){
-                rightDp[i]=energy[i];
-            }
-            else{
-                rightDp[i]=energy[i]+rightDp[i+k];
-            }
-            ans=max(ans,rightDp[i]);
-        }
-
         return ans;
     }
 };
