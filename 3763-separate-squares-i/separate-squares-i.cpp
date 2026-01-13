5eb6a5013b907f57572254bc67a68fd2a6bffeee
AJAJ ALAM <2020umt1399@mnit.ac.in>
2026-01-13 20:05:13 +0530

    Time: 112 ms (51.16%) | Memory: 201 MB (28.03%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.cpp
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -1,54 +1,60 @@
 class Solution {
 public:
     double separateSquares(vector<vector<int>>& squares) {
+        int n=squares.size();
 
-        double s = 1e18, e = -1e18;
+        double  s=1000000000.0;
+        double e =0;
 
-        for (auto &sq : squares) {
-            double y = sq[1];
-            double l = sq[2];
-            s = min(s, y);
-            e = max(e, y + l);
+        for(int i=0;i<n;i++){
+            double newy=(double)squares[i][1]+(double)squares[i][2];
+            double y = squares[i][1];
+            s=min(s,y);
+            e = max(e,newy);
         }
 
-        while (e - s > 1e-5) {
-            double mid = (s + e) / 2.0;
+        double ans = 0.0;
 
-            auto res = find(squares, mid);
+        while(e-s>1e-5){
+            double mid = (s+e)/2.0;
 
-            if (res[0] < res[1]) {
-                s = mid;
-            } else {
-                e = mid;
+            auto res = find(squares,mid);
+            if(res[0]<res[1]){
+                s=mid;
+            }
+            else{
+                e=mid;
             }
         }
 
         return s;
     }
 
-    vector<double> find(vector<vector<int>>& squares, double mid) {
+    vector<double> find(vector<vector<int>>& squares , double mid){
+        double left = 0.0;
+        double right=0.0;
 
-        double left = 0.0, right = 0.0;
+        for(int i=0;i<squares.size();i++){
+            int x = squares[i][0];
+            double y=(double)squares[i][1];
+            double l = (double)squares[i][2];
 
-        for (auto &sq : squares) {
-            double y = sq[1];
-            double l = sq[2];
-            double newY = y + l;
+            double newY=y+l;
 
-            if (y >= mid) {
-                right += l * l;
+            if(y>=mid){
+                right+=l*l;
             }
-            else if (newY <= mid) {
-                left += l * l;
+            else if(newY>mid && mid>y){
+                double ub = newY-mid;
+                double lb = mid-y;
+                right+=ub*l;
+                left+=lb*l;
             }
-            else {
-                double upper = newY - mid;
-                double lower = mid - y;
-                right += upper * l;
-                left  += lower * l;
+            else if(newY<=mid){
+                left+=l*l;
             }
         }
 
-        return {left, right};
+        return {left,right};
     }
 };
