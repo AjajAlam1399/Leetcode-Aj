b723de24ae347ee466201273d6cd359b05b1e8f0
AJAJ ALAM <2020umt1399@mnit.ac.in>
2026-05-12 22:45:33 +0530

    Time: 55 ms (24.46%) | Memory: 95.6 MB (50.31%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -6,9 +6,6 @@ class Solution {
             }
             return Integer.compare(a[1],b[1]);
         });
-        // for(int i=0;i<points.length;i++){
-        //     System.out.println(points[i][0] + ","+points[i][1]);
-        // }
         int ans=1;
         int n=points.length;
         int currMaxW=points[0][1];
