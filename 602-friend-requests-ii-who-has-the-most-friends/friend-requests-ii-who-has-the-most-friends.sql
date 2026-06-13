5998cd6f9c16de28251b9fcdeeb56de443db53e5
AJAJ ALAM <2020umt1399@mnit.ac.in>
2026-06-13 11:57:38 +0530

    Time: 365 ms (12.13%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-SELECT E.id,COUNT(E.id) as num FROM  (SELECT requester_id as id FROM RequestAccepted
-UNION ALL 
-SELECT accepter_id as id FROM RequestAccepted) E group by E.id ORDER BY num
-DESC limit 1;
+SELECT ids as id,count(ids) as num from (SELECT Q1.requester_id as ids from RequestAccepted Q1
+UNION ALL
+SELECT Q2.accepter_id as ids from RequestAccepted Q2) Q3 group by ids order by num desc limit 1;
